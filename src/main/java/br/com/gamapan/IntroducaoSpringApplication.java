754d867974e7b21658282a5f684387c83e3d2d6b
Weslley Lima <weslleyclima@yahoo.com.br>
2021-10-27 11:37:08 -0300

    Estruturação e configuração ao projeto inicial referente aula introdutória sobre Framework Spring Boot

---
+++ src/main/java/br/com/gamapan/IntroducaoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.gamapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoSpringApplication.class, args);
+	}
+
+}
